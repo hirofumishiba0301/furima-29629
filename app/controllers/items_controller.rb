6c2a8c58609c81afcb3e908eb3232f3befb68a99
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-10 22:38:25 +0900

    新規ユーザー登録機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
 end

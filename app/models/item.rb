b34c4363e9b9ba6bc4fb7079081b6ccd5b97bbaf
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-15 22:57:01 +0900

    itemコントローラー、ビューファイルの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :sale
+  belongs_to_active_hash :scheduled
+  belongs_to_active_hash :shippin
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :sale_id, numericality: { other_than: 1 } 
+  validates :shipping_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_id, numericality: { other_than: 1 } 
+  validates :price, presence: true,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
 end

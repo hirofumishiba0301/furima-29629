208791204f2158636efffb37b14bdd80cdcce3af
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-17 22:16:25 +0900

    ActiveStrage機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show, :search]
+
   def index
   end
 
@@ -18,7 +20,13 @@ class ItemsController < ApplicationController
 private
 
 def item_params
-  params.require(:item).permit(:image, :name, :info, :category_id, :prefecture_id, :sale_id, :scheduled_id, :shipping_id).merge(user_id: current_user.id)
+  params.require(:item).permit( :image, :name, :info, :category_id, :prefecture_id, :sale_id, :scheduled_id, :shipping_id, :price).merge(user_id: current_user.id)
 end
+
+  def move_to_index
+    unless user_signed_in?
+    redirect_to new_user_session_path
+    end
+  end
 
 end

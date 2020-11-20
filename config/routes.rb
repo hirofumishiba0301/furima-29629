7c3ff59927516754a4f2e888d5d0c61a81a866b2
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-20 23:41:20 +0900

    SoldOut表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
-  resources :items,only:[:new,:create]
+  resources :items,only:[:new,:create] do
+    resources :orders,only:[:new,:create]
+  end
 end

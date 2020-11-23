86c914b7591b303b95dd7092dfb66083af2cb7c6
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-23 22:54:43 +0900

    出品者以外の編集禁止機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to item_path,method: :get
+    end
   end
 
   def update

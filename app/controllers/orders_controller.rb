929125c9d0697363f0ea9a10e765c26ff1282524
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-11-26 23:47:58 +0900

    orderビュー、コントローラ設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.includes(:item)
+    @order = Order.includes(:item)
+    @item = Item.find(params[:item_id])
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :prefecture_id, :sale_id, :scheduled_id, :shipping_id, :price).merge(user_id: current_user.id)
+  end
+
 end
